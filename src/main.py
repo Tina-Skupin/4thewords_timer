e4cf30cef56bbc59b6485b3b6fe2b1134e46531f
Tina-Skupin <tskupin32@gmail.com>
2024-10-29 11:10:17 +0100

    calculations before timer coding

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 attack = 0
 mob_strength = 0
 
+#takes mob wordcount and attack value, returns words you actually have to type to defeat monster
 def calculate_actual_battle_count (attack, mob_strength):
     attack_bonus = attack/100
     attack_factor = 1+ attack_bonus
@@ -10,7 +11,9 @@ def calculate_actual_battle_count (attack, mob_strength):
     print (battle_count)
     return battle_count
 
+#recalculates words you need to beat the mob to minutes
+def words_to_minutes (battle_count):
+    minutes = battle_count *(60/1000)
+    print (minutes)
+    return minutes
 
-
-calculate_actual_battle_count (100, 1000)
-calculate_actual_battle_count (10, 1000)
--- tests/tests_main.py
+++ tests/tests_main.py
@@ -1,5 +1,5 @@
 import unittest
-from src.main import calculate_actual_battle_count  # import the function you want to test
+from src.main import calculate_actual_battle_count, words_to_minutes  # import the function you want to test
 
 class TestMain(unittest.TestCase):
     
@@ -10,6 +10,16 @@ class TestMain(unittest.TestCase):
     def test_act_battle_count2(self):
         result = calculate_actual_battle_count(10, 1000)
         self.assertEqual(result, 909)
+
+
+    def test_words_to_minutes1(self):
+        result = words_to_minutes(500)
+        self.assertEqual(result, 30)
+
+    def test_words_to_minutes2(self):
+        result = words_to_minutes(2000)
+        self.assertEqual(result, 120)
+
 
 if __name__ == '__main__':
     unittest.main()
